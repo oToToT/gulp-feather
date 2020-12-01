29e348c79207b9e1cdc595e76f2ee6ff6b6870ea
oToToT <ty1208chiang@gmail.com>
2020-12-01 21:41:53 +0800

    fit gulp guideline #2
    Currently, not support stream content

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
+const PLUGIN_NAME = 'gulp-feather';
+
+const PluginError = require('plugin-error');
 const through = require('through2');
 const { JSDOM } = require('jsdom');
 const { replace } = require('feather-icons');
 
 module.exports = (attrs = {}) => {
     return through.obj(function(file, encoding, cb) {
-        const dom = new JSDOM(file.contents, { runScripts: "outside-only" });
-
-        let saved_variables = new Map();
-
-        if (typeof document !== 'undefined') {
-            saved_variables.set('document', document);
-        }
-        document = dom.window.document;
-        if (typeof DOMParser !== 'undefined') {
-            saved_variables.set('DOMParser', DOMParser);
-        }
-        DOMParser = dom.window.DOMParser;
-
-        replace(attrs);
-
-        if (saved_variables.has('document')) {
-            document = saved_variables.get('document');
-        }
-        if (saved_variables.has('DOMParser')) {
-            document = saved_variables.get('DOMParser');
+        if (file.isNull()) {
+            // nothing to do
+            return callback(null, file);
         }
 
-        const result_html = dom.serialize();
-        if (file.contents.length < result_html.length) {
-            const buf = Buffer.from(result_html, encoding);
-            file.contents = buf;
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        } else if (file.isBuffer()) {
+            const dom = new JSDOM(file.contents, { runScripts: "outside-only" });
+
+            let saved_variables = new Map();
+
+            if (typeof document !== 'undefined') {
+                saved_variables.set('document', document);
+            }
+            document = dom.window.document;
+            if (typeof DOMParser !== 'undefined') {
+                saved_variables.set('DOMParser', DOMParser);
+            }
+            DOMParser = dom.window.DOMParser;
+
+            replace(attrs);
+
+            if (saved_variables.has('document')) {
+                document = saved_variables.get('document');
+            }
+            if (saved_variables.has('DOMParser')) {
+                document = saved_variables.get('DOMParser');
+            }
+
+            const result_html = dom.serialize();
+            if (file.contents.length < result_html.length) {
+                const buf = Buffer.from(result_html, encoding);
+                file.contents = buf;
+            } else {
+                file.contents.write(result_html, encoding);
+            }
+
+            return cb(null, file);
         } else {
-            file.contents.write(result_html, encoding);
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
         }
-        this.push(file);
-        return cb();
     });
 };

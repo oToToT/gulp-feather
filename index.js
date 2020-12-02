40bf594028be271209d4d77c18e324eb11ef83e6
oToToT <ty1208chiang@gmail.com>
2020-12-02 12:07:17 +0800

    slightliy modify code to be more robust

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const { JSDOM } = require('jsdom');
 const { replace } = require('feather-icons');
 
 module.exports = (attrs = {}) => through.obj(function (file, encoding, cb) {
+  const savedVariables = new Map();
+
+  function emulateBrowser(dom) {
+    // override document and DOMParser
+    if (typeof global.document !== 'undefined') {
+      savedVariables.set('document', global.document);
+    }
+    global.document = dom.window.document;
+    if (typeof global.DOMParser !== 'undefined') {
+      savedVariables.set('DOMParser', global.DOMParser);
+    }
+    global.DOMParser = dom.window.DOMParser;
+  }
+  function restoreVariables() {
+    // restore overrided variables
+    if (savedVariables.has('document')) {
+      global.document = savedVariables.get('document');
+    }
+    if (savedVariables.has('DOMParser')) {
+      global.DOMParser = savedVariables.get('DOMParser');
+    }
+  }
 
   if (file.isNull()) {
     // nothing to do
@@ -24,28 +46,12 @@ module.exports = (attrs = {}) => through.obj(function (file, encoding, cb) {
   if (file.isBuffer()) {
     // create dom from html file
     const dom = new JSDOM(file.contents, { runScripts: 'outside-only' });
-
-    // override document and DOMParser
-    const savedVariables = new Map();
-    if (typeof global.document !== 'undefined') {
-      savedVariables.set('document', global.document);
-    }
-    global.document = dom.window.document;
-    if (typeof global.DOMParser !== 'undefined') {
-      savedVariables.set('DOMParser', global.DOMParser);
-    }
-    global.DOMParser = dom.window.DOMParser;
-
+    // start emulation
+    emulateBrowser(dom);
     // replace feathericons
     replace(attrs);
-
-    // restore overrided variables
-    if (savedVariables.has('document')) {
-      global.document = savedVariables.get('document');
-    }
-    if (savedVariables.has('DOMParser')) {
-      global.DOMParser = savedVariables.get('DOMParser');
-    }
+    // stop emulation
+    restoreVariables();
 
     // output to file contents
     const HTMLResult = dom.serialize();
